da09e9b0c5a1c7d3a222c96203894ed39c19905a
Andrew Thomas Carroll <athomascarroll@gmail.com>
2023-05-17 22:33:26 -0700

    stuffSomewhere

--- index.js
+++ index.js
@@ -19,12 +19,29 @@ const PORT = process.env.PORT || 3001;
 
 // this creates the API endpoint localhost:3001/weather
 // (request, response) is required
-app.get('/weather', (request, response) => {
-  const {cityName} = request.query;
-  const cityWeather = weather.find(city => city.city_name.toLowerCase() === cityName.toLowerCase());
-  response.send(cityWeather);
-  console.log(request);
-});
+app.get('/weather', getWeather);
 
+function getWeather(request, response) {
+  const { cityName } = request.query;
+  const cityWeather = weather.find(city => city.city_name.toLowerCase() === cityName.toLowerCase());
+  try {
+    const weatherArray = cityWeather.data.map(day => new Forecast(day));
+    console.log('this is the weatherArray', weatherArray);
+    response.status(200).send(weatherArray);
+  } catch (error) {
+      errorHandler(error, response)
+  }
+}
+
+function Forecast(day) {
+  this.date = day.valid_date;
+  this.description = day.weather.description;
+}
+
+function errorHandler(error, response) {
+  console.log(error);
+  response.status(500).send('Error Error Error');
+
+}
 
 app.listen(PORT, () => console.log(`server listening on ${PORT}`));
